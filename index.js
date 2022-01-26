3605ad470d77b3de9c516830c6eb872b0194cb75
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-01-26 08:07:08 +0000

    solution

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var minMovesToSeat = function(seats, students) {
+    seats.sort((a,b) => a - b);
+    students.sort((a,b) => a - b);
+
+    let count = 0;
+    for(let i=0; i<seats.length; i++){
+       
+        count += Math.abs(seats[i] - students[i]);
+   
+    }
+
+    return count;
+
+
 }
 
 /*
